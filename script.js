1741797771ca1e43b3185308041a0b2e6c163337
Sachin <vsharma3973@gmail.com>
2020-08-30 05:04:41 +0530

    fixed the scrolling

--- script.js
+++ script.js
@@ -36,10 +36,22 @@ function setTheme(mode) {
   localStorage.setItem("theme", mode);
 }
 
-window.scrollTo(0, 0);
-
+// Scroll to specific values
+// scrollTo is the same
 window.scroll({
-  top: 0,
+  top: 2500,
   left: 0,
+  behavior: "smooth",
+});
+
+// Scroll certain amounts from current position
+window.scrollBy({
+  top: 100, // could be negative value
+  left: 0,
+  behavior: "smooth",
+});
+
+// Scroll to a certain element
+document.querySelector("#hello").scrollIntoView({
   behavior: "smooth",
 });
